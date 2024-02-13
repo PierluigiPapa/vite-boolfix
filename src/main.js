4b44df13f82feb48b1e2dba8e20852228927851a
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-13 13:16:29 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
