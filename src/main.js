03a424dfc8de222eb78bae5997b74cece4559912
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-13 13:23:48 +0100

    workspace setup initial

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar, faSearch } from '@fortawesome/free-solid-svg-icons'
 
-
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(faStar, faSearch)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
